0ff244500eb3590a64b49941b5796667027c566a
Abizer Nasir <abizern@abizern.org>
2022-05-06 13:33:07 +0100

    Add explanatory comments to extensions in Preview Content directory

--- SimpleToDo/SimpleToDo/Preview Content/ToDoItem+Preview.swift
+++ SimpleToDo/SimpleToDo/Preview Content/ToDoItem+Preview.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+/// Code in this folder is not included when creating an archive to send to the app store, in the same way that the code for SwifUI Previews are stripped out of archived builds
 extension ToDoItem {
     /// An example property that's used for Xcode previewing.
     static let example = ToDoItem()
